1c6c859c5fa97d88aa753525d39b545473e510d6
jaebradley <jae.b.bradley@gmail.com>
2016-09-01 23:16:47 -0400

    add player test cases

--- tests/test_player.py
+++ tests/test_player.py
@@ -5,6 +5,15 @@ from stats.data.team import Team
 
 
 class TestPlayer(TestCase):
+    def test_instantiation(self):
+        player = Player("bae jadley", Team.get_id(Team.boston_celtics), 1)
+        self.assertEqual(player.name, "bae jadley")
+        self.assertEqual(player.team, Team.boston_celtics)
+        self.assertEqual(player.nba_id, 1)
+
+    def test_get_query_parameter_name(self):
+        self.assertEqual(Player.get_query_parameter_name(), "PlayerId")
+
     def test_create(self):
         player = Player.create("bae jadley", Team.get_id(Team.boston_celtics), 1)
         self.assertEqual(player.name, "bae jadley")
