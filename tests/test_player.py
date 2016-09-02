2bc7bd52c606eb8048996eeb47eaac0212dda97e
jaebradley <jae.b.bradley@gmail.com>
2016-09-01 23:31:10 -0400

    fix player test

--- tests/test_player.py
+++ tests/test_player.py
@@ -6,7 +6,7 @@ from stats.data.team import Team
 
 class TestPlayer(TestCase):
     def test_instantiation(self):
-        player = Player("bae jadley", Team.get_id(Team.boston_celtics), 1)
+        player = Player("bae jadley", Team.boston_celtics, 1)
         self.assertEqual(player.name, "bae jadley")
         self.assertEqual(player.team, Team.boston_celtics)
         self.assertEqual(player.nba_id, 1)
