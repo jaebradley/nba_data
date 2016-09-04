4d4be4698f24b1e026c3fef8249e25e426b220d8
jaebradley <jae.b.bradley@gmail.com>
2016-09-04 18:33:01 -0400

    add tests

---
+++ stats/data/test_advancedPlayerBoxScore.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+from unittest import TestCase
+
+from stats.data.advanced_player_box_score import AdvancedPlayerBoxScore
+from stats.data.team import Team
+
+
+class TestAdvancedPlayerBoxScore(TestCase):
+    def test_create(self):
+        test_player_name = "jae"
+        test_player_nba_id = 1
+        test_team_nba_id = 1610612738
+        box_score = AdvancedPlayerBoxScore.create(player_name=test_player_name, player_nba_id=test_player_nba_id,
+                                                  team_id=test_team_nba_id, comment="bae jadley", seconds_played=123,
+                                                  offensive_rating=Decimal("110.1"), defensive_rating=Decimal("110.2"),
+                                                  teammate_assist_percentage=Decimal("50.0"), assist_to_turnover_ratio=Decimal("2.1"),
+                                                  assists_per_100_possessions=Decimal("41.4"), offensive_rebound_percentage=Decimal("12.3"),
+                                                  defensive_rebound_percentage=Decimal("45.6"), turnovers_per_100_possessions=Decimal("78.9"),
+                                                  effective_field_goal_percentage=Decimal("10.1"), true_shooting_percentage=Decimal("4.56"),
+                                                  usage_percentage=Decimal("7.89"))
+
+        self.assertEqual(box_score.player.name, "jae")
+        self.assertEqual(box_score.player.nba_id, test_player_nba_id)
+        self.assertEqual(box_score.player.team, Team.boston_celtics)
+        self.assertEqual(box_score.comment, "bae jadley")
+        self.assertEqual(box_score.seconds_played, 123)
+        self.assertEqual(box_score.offensive_rating, Decimal("110.1"))
+        self.assertEqual(box_score.defensive_rating, Decimal("110.2"))
+        self.assertEqual(box_score.teammate_assist_percentage, Decimal("50.0"))
+        self.assertEqual(box_score.assist_to_turnover_ratio, Decimal("2.1"))
+        self.assertEqual(box_score.assists_per_100_possessions, Decimal("41.4"))
+        self.assertEqual(box_score.offensive_rebound_percentage, Decimal("12.3"))
+        self.assertEqual(box_score.defensive_rebound_percentage, Decimal("45.6"))
+        self.assertEqual(box_score.turnovers_per_100_possessions, Decimal("78.9"))
+        self.assertEqual(box_score.effective_field_goal_percentage, Decimal("10.1"))
+        self.assertEqual(box_score.true_shooting_percentage, Decimal("4.56"))
+        self.assertEqual(box_score.usage_percentage, Decimal("7.89"))
