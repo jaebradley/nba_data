6cb0b33f57b2e00d213552f317a461f94dc6553d
jaebradley <jae.b.bradley@gmail.com>
2016-09-07 01:00:05 -0400

    traditional team test

--- stats/data/traditional_team_box_score.py
+++ stats/data/traditional_team_box_score.py
@@ -1,11 +1,12 @@
 from team import Team
+from unit import Unit
 
 
 class TraditionalTeamBoxScore:
     def __init__(self, team, seconds_played, unit, field_goals_made, field_goal_attempts,
                  three_point_field_goals_made, three_point_field_goal_attempts,
                  free_throws_made, free_throws_attempts, offensive_rebounds, defensive_rebounds, assists,
-                 steals, blocks, turnovers, personal_fouls, plus_minus):
+                 steals, blocks, turnovers, personal_fouls):
         self.team = team
         self.seconds_played = seconds_played
         self.unit = unit
@@ -22,15 +23,16 @@ class TraditionalTeamBoxScore:
         self.blocks = blocks
         self.turnovers = turnovers
         self.personal_fouls = personal_fouls
-        self.plus_minus = plus_minus
 
     @staticmethod
-    def create(team_id, unit, seconds_played, field_goals_made, field_goal_attempts,
+    def create(team_id, unit_name, seconds_played, field_goals_made, field_goal_attempts,
                three_point_field_goals_made, three_point_field_goal_attempts,
                free_throws_made, free_throws_attempts, offensive_rebounds, defensive_rebounds, assists,
-               steals, blocks, turnovers, personal_fouls, plus_minus):
+               steals, blocks, turnovers, personal_fouls):
         return TraditionalTeamBoxScore(team=Team.get_team_by_id(team_id=team_id),
-                                       unit=unit, seconds_played=seconds_played, field_goals_made=field_goals_made,
+                                       unit=Unit.get_unit_from_name(unit_name=unit_name),
+                                       seconds_played=seconds_played,
+                                       field_goals_made=field_goals_made,
                                        field_goal_attempts=field_goal_attempts,
                                        three_point_field_goals_made=three_point_field_goals_made,
                                        three_point_field_goal_attempts=three_point_field_goal_attempts,
@@ -42,5 +44,4 @@ class TraditionalTeamBoxScore:
                                        steals=steals,
                                        blocks=blocks,
                                        turnovers=turnovers,
-                                       personal_fouls=personal_fouls,
-                                       plus_minus=plus_minus)
+                                       personal_fouls=personal_fouls)
---
+++ tests/test_traditionalTeamBoxScore.py
@@ -0,0 +1,35 @@
+from unittest import TestCase
+
+from stats.data.team import Team
+from stats.data.unit import Unit
+from stats.data.traditional_team_box_score import TraditionalTeamBoxScore
+
+
+class TestTraditionalTeamBoxScore(TestCase):
+    def test_create(self):
+        test_team_id = 1610612738
+        box_score = TraditionalTeamBoxScore.create(team_id=test_team_id, seconds_played=1, unit_name="Starters",
+                                                   field_goals_made=2, field_goal_attempts=3,
+                                                   three_point_field_goals_made=4,
+                                                   three_point_field_goal_attempts=5, free_throws_made=6,
+                                                   free_throws_attempts=7,
+                                                   offensive_rebounds=8, defensive_rebounds=9, steals=10, blocks=11,
+                                                   turnovers=12, personal_fouls=13, assists=14)
+
+        self.assertIsNotNone(box_score)
+        self.assertEqual(box_score.team, Team.boston_celtics)
+        self.assertEqual(box_score.seconds_played, 1)
+        self.assertEqual(box_score.unit, Unit.starters)
+        self.assertEqual(box_score.field_goals_made, 2)
+        self.assertEqual(box_score.field_goal_attempts, 3)
+        self.assertEqual(box_score.three_point_field_goals_made, 4)
+        self.assertEqual(box_score.three_point_field_goal_attempts, 5)
+        self.assertEqual(box_score.free_throws_made, 6)
+        self.assertEqual(box_score.free_throws_attempts, 7)
+        self.assertEqual(box_score.offensive_rebounds, 8)
+        self.assertEqual(box_score.defensive_rebounds, 9)
+        self.assertEqual(box_score.steals, 10)
+        self.assertEqual(box_score.blocks, 11)
+        self.assertEqual(box_score.turnovers, 12)
+        self.assertEqual(box_score.personal_fouls, 13)
+        self.assertEqual(box_score.assists, 14)
