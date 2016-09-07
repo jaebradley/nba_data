aba14b7fe11bf9e6b688b53adf4d71d5e3074f64
jaebradley <jae.b.bradley@gmail.com>
2016-09-06 21:46:49 -0400

    advanced box score deserializer test

--- tests/test_advancedBoxScoreDeserializer.py
+++ tests/test_advancedBoxScoreDeserializer.py
@@ -1,7 +1,9 @@
+from decimal import Decimal
 import json
 import os
 from unittest import TestCase
 
+from stats.data.team import Team
 from stats.client.deserializers.advanced_box_score_deserializer import AdvancedBoxScoreDeserializer
 from tests.config import ROOT_DIRECTORY
 
@@ -15,3 +17,37 @@ class TestAdvancedBoxScoreDeserializer(TestCase):
             data = json.load(data_file)
             advanced_box_score = AdvancedBoxScoreDeserializer.deserialize_advanced_box_score(data)
             self.assertEqual(advanced_box_score.game_id, "0021501205")
+            self.assertEqual(len(advanced_box_score.player_box_scores), 26)
+
+            nicolas_batum_box_score = advanced_box_score.player_box_scores[0]
+            self.assertEqual(nicolas_batum_box_score.player.nba_id, 201587)
+            self.assertEqual(nicolas_batum_box_score.player.name, "Nicolas Batum")
+            self.assertEqual(nicolas_batum_box_score.player.team, Team.charlotte_hornets)
+            self.assertEqual(nicolas_batum_box_score.comment, "")
+            self.assertEqual(nicolas_batum_box_score.seconds_played, 1037)
+            self.assertEqual(nicolas_batum_box_score.offensive_rating, Decimal('143.3'))
+            self.assertEqual(nicolas_batum_box_score.defensive_rating, Decimal('54.4'))
+            self.assertEqual(nicolas_batum_box_score.teammate_assist_percentage, Decimal('35.7'))
+            self.assertEqual(nicolas_batum_box_score.assist_to_turnover_ratio, Decimal('5.0'))
+            self.assertEqual(nicolas_batum_box_score.assists_per_100_possessions, Decimal('41.7'))
+            self.assertEqual(nicolas_batum_box_score.offensive_rebound_percentage, Decimal('0.0'))
+            self.assertEqual(nicolas_batum_box_score.defensive_rebound_percentage, Decimal('17.6'))
+            self.assertEqual(nicolas_batum_box_score.turnovers_per_100_possessions, Decimal('8.3'))
+            self.assertEqual(nicolas_batum_box_score.effective_field_goal_percentage, Decimal('50.0'))
+            self.assertEqual(nicolas_batum_box_score.true_shooting_percentage, Decimal('50.0'))
+            self.assertEqual(nicolas_batum_box_score.usage_percentage, Decimal('18.1'))
+
+            self.assertEqual(len(advanced_box_score.team_box_scores), 2)
+            charlotte_hornets_box_score = advanced_box_score.team_box_scores[0]
+            self.assertEqual(charlotte_hornets_box_score.team, Team.charlotte_hornets)
+            self.assertEqual(charlotte_hornets_box_score.seconds_played, 14400)
+            self.assertEqual(charlotte_hornets_box_score.offensive_rating, Decimal("116.7"))
+            self.assertEqual(charlotte_hornets_box_score.defensive_rating, Decimal("99.2"))
+            self.assertEqual(charlotte_hornets_box_score.teammate_assist_percentage, Decimal("56.1"))
+            self.assertEqual(charlotte_hornets_box_score.assist_to_turnover_ratio, Decimal("1.6"))
+            self.assertEqual(charlotte_hornets_box_score.assists_per_100_possessions, Decimal("18.0"))
+            self.assertEqual(charlotte_hornets_box_score.offensive_rebound_percentage, Decimal("17.5"))
+            self.assertEqual(charlotte_hornets_box_score.defensive_rebound_percentage, Decimal("74.0"))
+            self.assertEqual(charlotte_hornets_box_score.turnovers_per_100_possessions, Decimal("14.3"))
+            self.assertEqual(charlotte_hornets_box_score.effective_field_goal_percentage, Decimal("59.3"))
+            self.assertEqual(charlotte_hornets_box_score.true_shooting_percentage, Decimal("62.9"))
