cd1be339e9af6010aacbd0cf0d90e26da3d50e62
jaebradley <jae.b.bradley@gmail.com>
2016-09-08 23:25:28 -0400

    real setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'nba_data',
   packages = ['nba_data'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.2',
   description = 'A nba stats client',
   author = 'Jae Bradley',
   author_email = 'jae.b.bradley@gmail.com',
-  url = 'https://github.com/jbradley/nba_data', # use the URL to the github repo
-  download_url = 'https://github.com/jbradley/nba_data/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/jaebradley/nba_data', # use the URL to the github repo
+  download_url = 'https://github.com/jaebradley/nba_data/tarball/0.1', # I'll explain this in a second
   keywords = ['nba', 'stats'], # arbitrary keywords
   classifiers = [],
 )
