2642d68c9d326b5aee1cbb43e6ef4af8a7812fc4
jaebradley <jae.b.bradley@gmail.com>
2016-09-11 22:38:19 -0400

    udpated setup version

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'nba_data',
   packages = find_packages(exclude=['tests*']),
   install_requires=['requests', 'enum34'],
-  version = '0.4',
+  version = '0.5',
   description = 'A nba nba_data client',
   author = 'Jae Bradley',
   author_email = 'jae.b.bradley@gmail.com',
   url = 'https://github.com/jaebradley/nba_data', # use the URL to the github repo
-  download_url = 'https://github.com/jaebradley/nba_data/tarball/0.6', # I'll explain this in a second
+  download_url = 'https://github.com/jaebradley/nba_data/tarball/0.7', # I'll explain this in a second
   keywords = ['nba', 'nba_data'], # arbitrary keywords
   classifiers = [],
 )
