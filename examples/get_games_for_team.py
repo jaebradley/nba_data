623f0b7dd09f0e4b2cf96562ddd62dc62f3ac9ff
jaebradley <jae.b.bradley@gmail.com>
2016-09-17 13:16:23 -0400

    update tests

--- examples/get_games_for_team.py
+++ examples/get_games_for_team.py
@@ -1,7 +1,7 @@
-from nba_data.client import Client
-from nba_data.data.season import Season
-from nba_data.data.season_type import SeasonType
-from nba_data.data.team import Team
+from nba_data import Client
+from nba_data import Season
+from nba_data import SeasonType
+from nba_data import Team
 
 
 def get_regular_season_games_for_2015_boston_celtics():
