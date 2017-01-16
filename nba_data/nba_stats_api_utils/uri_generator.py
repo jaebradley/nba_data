a4bf99b7192a8a8819406b962345f1ad3f0cfd92
jaebradley <jae.b.bradley@gmail.com>
2017-01-16 17:37:16 -0500

    rename stats base uri

--- nba_data/nba_stats_api_utils/uri_generator.py
+++ nba_data/nba_stats_api_utils/uri_generator.py
@@ -1,5 +1,5 @@
 class UriGenerator:
-    base_uri = "http://stats.nba.com/stats/"
+    stats_base_uri = "http://stats.nba.com/stats/"
     common_all_players_path = "commonallplayers"
     team_game_log_path = "teamgamelog"
     common_player_info_path = "commonplayerinfo"
@@ -11,20 +11,20 @@ class UriGenerator:
 
     @staticmethod
     def generate_common_all_players_uri():
-        return UriGenerator.base_uri + UriGenerator.common_all_players_path
+        return UriGenerator.stats_base_uri + UriGenerator.common_all_players_path
 
     @staticmethod
     def generate_team_game_log_uri():
-        return UriGenerator.base_uri + UriGenerator.team_game_log_path
+        return UriGenerator.stats_base_uri + UriGenerator.team_game_log_path
 
     @staticmethod
     def generate_common_player_info_uri():
-        return UriGenerator.base_uri + UriGenerator.common_player_info_path
+        return UriGenerator.stats_base_uri + UriGenerator.common_player_info_path
 
     @staticmethod
     def generate_advanced_box_score_uri():
-        return UriGenerator.base_uri + UriGenerator.advanced_box_score_path
+        return UriGenerator.stats_base_uri + UriGenerator.advanced_box_score_path
 
     @staticmethod
     def generate_traditional_box_score_uri():
-        return UriGenerator.base_uri + UriGenerator.traditional_box_score_path
+        return UriGenerator.stats_base_uri + UriGenerator.traditional_box_score_path
