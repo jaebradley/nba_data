d228016ad4b34456fafa2847f7d350208a86b317
jaebradley <jae.b.bradley@gmail.com>
2017-01-16 17:40:59 -0500

    add uri generator method for calendar data uri

--- nba_data/nba_stats_api_utils/uri_generator.py
+++ nba_data/nba_stats_api_utils/uri_generator.py
@@ -1,10 +1,13 @@
 class UriGenerator:
     stats_base_uri = "http://stats.nba.com/stats/"
+    data_base_uri = "http://data.nba.net/"
+
     common_all_players_path = "commonallplayers"
     team_game_log_path = "teamgamelog"
     common_player_info_path = "commonplayerinfo"
     advanced_box_score_path = "boxscoreadvancedv2"
     traditional_box_score_path = "boxscoretraditionalv2"
+    calendar_path = "calendar"
 
     def __init__(self):
         pass
@@ -28,3 +31,11 @@ class UriGenerator:
     @staticmethod
     def generate_traditional_box_score_uri():
         return UriGenerator.stats_base_uri + UriGenerator.traditional_box_score_path
+
+    @staticmethod
+    def generate_data_uri(path):
+        return UriGenerator.data_base_uri + "/data/10s/prod/v1/" + path
+
+    @staticmethod
+    def generate_calendar_data_uri():
+        return UriGenerator.generate_data_uri(UriGenerator.calendar_path)
