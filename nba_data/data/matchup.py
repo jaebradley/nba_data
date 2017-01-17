b0b01b9b818f086c2d3b105de5aa8c79be658ba2
jaebradley <jae.b.bradley@gmail.com>
2017-01-16 20:59:54 -0500

    add scoreboard deserializer

--- nba_data/data/matchup.py
+++ nba_data/data/matchup.py
@@ -7,6 +7,14 @@ class Matchup:
         self.away_team = away_team
 
     @staticmethod
+    def create(home_team_id, away_team_id):
+        assert isinstance(home_team_id, int)
+        assert isinstance(away_team_id, int)
+
+        return Matchup(home_team=Team.get_team_by_id(team_id=home_team_id),
+                       away_team=Team.get_team_by_id(team_id=away_team_id))
+
+    @staticmethod
     def create(home_team_abbreviation, away_team_abbreviation):
         return Matchup(home_team=Team.get_team_by_abbreviation(home_team_abbreviation),
                        away_team=Team.get_team_by_abbreviation(away_team_abbreviation))
