6e50536cba32d909ee464fce670f1f74ff4e23e0
jaebradley <jae.b.bradley@gmail.com>
2017-01-16 23:53:17 -0500

    update package version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'nba_data',
   packages = find_packages(exclude=['tests*']),
   install_requires=['requests', 'enum34'],
-  version = '1.0',
+  version = '1.1',
   description = 'A nba stats client',
   author = 'Jae Bradley',
   author_email = 'jae.b.bradley@gmail.com',
