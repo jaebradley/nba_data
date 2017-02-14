24af5857f26aecad8a893a11b166d64192d23339
jaebradley <jae.b.bradley@gmail.com>
2017-02-14 09:59:36 -0500

    update spacing

--- nba_data/data/advanced_player_box_score.py
+++ nba_data/data/advanced_player_box_score.py
@@ -32,8 +32,13 @@ class AdvancedPlayerBoxScore:
         return AdvancedPlayerBoxScore(player=BoxScorePlayer.create(name=player_name, team_id=team_id, id=player_id,
                                                                    status=PlayerStatus.from_comment(comment=comment)),
                                       seconds_played=seconds_played, offensive_rating=offensive_rating,
-                                      defensive_rating=defensive_rating, teammate_assist_percentage=teammate_assist_percentage,
-                                      assist_to_turnover_ratio=assist_to_turnover_ratio, assists_per_100_possessions=assists_per_100_possessions,
-                                      offensive_rebound_percentage=offensive_rebound_percentage, defensive_rebound_percentage=defensive_rebound_percentage,
-                                      turnovers_per_100_possessions=turnovers_per_100_possessions, effective_field_goal_percentage=effective_field_goal_percentage,
-                                      true_shooting_percentage=true_shooting_percentage, usage_percentage=usage_percentage)
+                                      defensive_rating=defensive_rating,
+                                      teammate_assist_percentage=teammate_assist_percentage,
+                                      assist_to_turnover_ratio=assist_to_turnover_ratio,
+                                      assists_per_100_possessions=assists_per_100_possessions,
+                                      offensive_rebound_percentage=offensive_rebound_percentage,
+                                      defensive_rebound_percentage=defensive_rebound_percentage,
+                                      turnovers_per_100_possessions=turnovers_per_100_possessions,
+                                      effective_field_goal_percentage=effective_field_goal_percentage,
+                                      true_shooting_percentage=true_shooting_percentage,
+                                      usage_percentage=usage_percentage)
