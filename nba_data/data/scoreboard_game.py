760503e15efaaa13b7f1918f63fbf48dd483c33b
jaebradley <jae.b.bradley@gmail.com>
2017-02-15 12:38:47 -0500

    remove assertions

--- nba_data/data/scoreboard_game.py
+++ nba_data/data/scoreboard_game.py
@@ -1,8 +1,3 @@
-from datetime import datetime
-
-from nba_data.data.matchup import MatchUp
-from nba_data.data.season import Season
-
 # TODO: @jbradley in the future, reconcile / deprecate Game class
 # TODO: @jbradley currently this class is fulfilling needs of dfs_site_data project - in the future, add more fields
 
@@ -10,11 +5,6 @@ from nba_data.data.season import Season
 class ScoreboardGame:
 
     def __init__(self, id, season, start_time, match_up):
-        assert isinstance(id, basestring)
-        assert isinstance(season, Season)
-        assert isinstance(start_time, datetime)
-        assert isinstance(match_up, MatchUp)
-
         self.id = id
         self.season = season
         self.start_time = start_time
