fe6da741cfba7c4ea6cb6af32feec6cd9ca4f4b5
jaebradley <jae.b.bradley@gmail.com>
2017-02-16 15:35:24 -0500

    move advanced box scores to one file

--- nba_data/data/advanced_box_score.py
+++ nba_data/data/advanced_box_score.py
@@ -40,3 +40,48 @@ class AdvancedBoxScore:
 
     def get_additional_unicode(self):
         return NotImplementedError('Should be implemented in concrete class')
+
+
+class AdvancedPlayerBoxScore(AdvancedBoxScore):
+    def __init__(self, player, seconds_played, offensive_rating, defensive_rating,
+                 teammate_assist_percentage, assist_to_turnover_ratio, assists_per_100_possessions,
+                 offensive_rebound_percentage, defensive_rebound_percentage, turnovers_per_100_possessions,
+                 effective_field_goal_percentage, true_shooting_percentage, usage_percentage):
+        self.player = player
+        self.usage_percentage = usage_percentage
+        AdvancedBoxScore.__init__(self, seconds_played=seconds_played,
+                                  offensive_rating=offensive_rating, defensive_rating=defensive_rating,
+                                  teammate_assist_percentage=teammate_assist_percentage,
+                                  assist_to_turnover_ratio=assist_to_turnover_ratio,
+                                  assists_per_100_possessions=assists_per_100_possessions,
+                                  offensive_rebound_percentage=offensive_rebound_percentage,
+                                  defensive_rebound_percentage=defensive_rebound_percentage,
+                                  turnovers_per_100_possessions=turnovers_per_100_possessions,
+                                  effective_field_goal_percentage=effective_field_goal_percentage,
+                                  true_shooting_percentage=true_shooting_percentage)
+
+    def get_additional_unicode(self):
+        return 'player: {player} | usage percentage: {usage_percentage}'.format(player=self.player,
+                                                                                usage_percentage=self.usage_percentage)
+
+
+class AdvancedTeamBoxScore(AdvancedBoxScore):
+    def __init__(self, team, seconds_played, offensive_rating, defensive_rating,
+                 teammate_assist_percentage, assist_to_turnover_ratio, assists_per_100_possessions,
+                 offensive_rebound_percentage, defensive_rebound_percentage, turnovers_per_100_possessions,
+                 effective_field_goal_percentage, true_shooting_percentage):
+        self.team = team
+        AdvancedBoxScore.__init__(self, seconds_played=seconds_played,
+                                  offensive_rating=offensive_rating, defensive_rating=defensive_rating,
+                                  teammate_assist_percentage=teammate_assist_percentage,
+                                  assist_to_turnover_ratio=assist_to_turnover_ratio,
+                                  assists_per_100_possessions=assists_per_100_possessions,
+                                  offensive_rebound_percentage=offensive_rebound_percentage,
+                                  defensive_rebound_percentage=defensive_rebound_percentage,
+                                  turnovers_per_100_possessions=turnovers_per_100_possessions,
+                                  effective_field_goal_percentage=effective_field_goal_percentage,
+                                  true_shooting_percentage=true_shooting_percentage)
+
+    def get_additional_unicode(self):
+        return 'team: {team}'.format(team=self.team)
+
