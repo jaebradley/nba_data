86a927675a5cf5c5c9444e3b446bce4a2e7a677b
jaebradley <jae.b.bradley@gmail.com>
2017-02-16 10:13:54 -0500

    remove unnecessary method

--- nba_data/data/advanced_player_box_score.py
+++ nba_data/data/advanced_player_box_score.py
@@ -1,5 +1,3 @@
-from box_score_player import BoxScorePlayer
-from player_status import PlayerStatus
 from nba_data.data.advanced_box_score import AdvancedBoxScore
 
 
@@ -24,22 +22,3 @@ class AdvancedPlayerBoxScore(AdvancedBoxScore):
     def get_additional_unicode(self):
         return 'player: {player} | usage percentage: {usage_percentage}'.format(player=self.player,
                                                                                 usage_percentage=self.usage_percentage)
-
-    @staticmethod
-    def create(player_name, player_id, team_id, comment, seconds_played, offensive_rating, defensive_rating,
-               teammate_assist_percentage, assist_to_turnover_ratio, assists_per_100_possessions,
-               offensive_rebound_percentage, defensive_rebound_percentage, turnovers_per_100_possessions,
-               effective_field_goal_percentage, true_shooting_percentage, usage_percentage):
-        return AdvancedPlayerBoxScore(player=BoxScorePlayer.create(name=player_name, team_id=team_id, id=player_id,
-                                                                   status=PlayerStatus.from_comment(comment=comment)),
-                                      seconds_played=seconds_played, offensive_rating=offensive_rating,
-                                      defensive_rating=defensive_rating,
-                                      teammate_assist_percentage=teammate_assist_percentage,
-                                      assist_to_turnover_ratio=assist_to_turnover_ratio,
-                                      assists_per_100_possessions=assists_per_100_possessions,
-                                      offensive_rebound_percentage=offensive_rebound_percentage,
-                                      defensive_rebound_percentage=defensive_rebound_percentage,
-                                      turnovers_per_100_possessions=turnovers_per_100_possessions,
-                                      effective_field_goal_percentage=effective_field_goal_percentage,
-                                      true_shooting_percentage=true_shooting_percentage,
-                                      usage_percentage=usage_percentage)
