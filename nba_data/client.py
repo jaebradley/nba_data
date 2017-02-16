638ebe0e497117ea0047adb4c6c2b3c77567c7ca
jaebradley <jae.b.bradley@gmail.com>
2017-02-16 16:02:43 -0500

    update common player info  deserializer file name

--- nba_data/client.py
+++ nba_data/client.py
@@ -7,7 +7,7 @@ from nba_data.data.season import Season
 from nba_data.data.season_type import SeasonType
 from nba_data.deserializers.calendar import CalendarDeserializer
 from nba_data.deserializers.common_all_players_deserializer import CommonAllPlayersDeserializer
-from nba_data.deserializers.common_player_info_deserializer import CommonPlayerInfoDeserializer
+from nba_data.deserializers.common_player_info import CommonPlayerInfoDeserializer
 from nba_data.deserializers.scoreboard import ScoreboardDeserializer
 from nba_data.deserializers.season_players import SeasonPlayersDeserializer
 from nba_data.deserializers.team_game_log import TeamGameLogDeserializer
