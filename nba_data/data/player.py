bd8493f464d46d9a1b7a4dadf98d584627131945
jaebradley <jae.b.bradley@gmail.com>
2017-02-16 15:24:00 -0500

    implement player types

--- nba_data/data/player.py
+++ nba_data/data/player.py
@@ -26,8 +26,11 @@ class TeamPlayer(Player):
         self.team = team
         Player.__init__(self, name, id)
 
+    def get_base_unicode(self):
+        return 'team: {team} | {base_unicode}'.format(team=self.team, base_unicode=Player.get_base_unicode(self))
+
     def get_additional_unicode(self):
-        return 'team: {team}'.format(team=self.team)
+        raise NotImplementedError('Implement in concrete classes')
 
 
 class SeasonPlayer(Player):
@@ -41,10 +44,35 @@ class SeasonPlayer(Player):
                                                                         team_seasons=self.team_seasons)
 
 
+class CommonAllPlayer(TeamPlayer):
+    def __init__(self, name, id, team):
+        TeamPlayer.__init__(self, name, id, team)
+
+    def get_additional_unicode(self):
+        return ''
+
+
+class DetailedPlayer(TeamPlayer):
+
+    def __init__(self, name, team, id, birth_date, height, weight, jersey, position):
+        self.birth_date = birth_date
+        self.height = height
+        self.weight = weight
+        self.jersey = jersey
+        self.position = position
+        self.jersey = jersey
+        TeamPlayer.__init__(self, name=name, id=id, team=team)
+
+    def get_additional_unicode(self):
+        return 'birth date: {birth_date} | height: {height} | weight: {weight} | ' 'jersey: {jersey} | ' \
+               'position: {position}'.format(birth_data=self.birth_date, height=self.height, weight=self.weight,
+                                             jersey=self.jersey, position=self.position)
+
+
 class BoxScorePlayer(TeamPlayer):
     def __init__(self, name, team, id, status):
         self.status = status
         TeamPlayer.__init__(self, name=name, id=id, team=team)
 
     def get_additional_unicode(self):
-        return 'team: {team} | status: {status}'.format(team=self.team, status=self.status)
+        return 'status: {status}'.format(status=self.status)
