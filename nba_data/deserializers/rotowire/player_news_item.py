3cfa38c1d604018136efbe767a0edb6bb5d31d4e
jaebradley <jae.b.bradley@gmail.com>
2017-04-29 00:36:39 -0400

    deserializer

---
+++ nba_data/deserializers/rotowire/player_news_item.py
@@ -0,0 +1,27 @@
+
+
+class RotowirePlayerNewsItemDeserializer:
+    caption_field_name = 'ListItemCaption'
+    description_field_name = 'ListItemDescription'
+    list_item_publish_date_field_name = 'ListItemPubDate'
+    last_updated_field_name = 'lastUpdate'
+    update_id_field_name = 'UpdateId'
+    roto_id_field_name = 'RotoId'
+    player_id_field_name = 'PlayerID'
+    first_name_field_name = 'FirstName'
+    last_name_field_name = 'LastName'
+    position_abbreviation_field_name = 'Position'
+    team_abbreviation_field_name = 'Team'
+    team_code_field_name = 'TeamCode'
+    date_field_name = 'Date'
+    priority_field_name = 'Priority'
+    player_code_field_name = 'player_code'
+    headline_field_name = 'Headline'
+    injured_field_name = 'Injured'
+    injured_status_field_name = 'Injured_Status'
+    injury_location_field_name = 'Injury_Location'
+    injury_type_field_name = 'Injury_Type'
+    injury_detail_field_name = 'Injury_Detail'
+    injury_side_field_name = 'Injury_Side'
+
+    
